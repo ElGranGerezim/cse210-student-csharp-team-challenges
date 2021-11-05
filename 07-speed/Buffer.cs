9e98868f4f438cdc9a131356f5f300a74e4be1ca
faraguti <faraguti@gmail.com>
2021-11-04 18:03:32 -0600

    Buffer - Felipe

--- 07-speed/Buffer.cs
+++ 07-speed/Buffer.cs
@@ -4,19 +4,22 @@ namespace _07_speed
 {
     class Buffer : Actor
     {
+        // Empties the text field for the next word.
         public void reset()
         {
-
+            _text = "";
         }
 
+        // Returns the comparison of the input with the word.
         public bool compare(string word)
         {
-
+            return _text.Contains(word);
         }
 
+        // Update the text with the user input.
         public void updateBuffer(string input)
         {
-            
+            _text += input;
         }
     }
 
